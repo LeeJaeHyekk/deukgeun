a5ca511d86ba734af6341f0fdb6d6c0ea879ea50
JaeHyuk <111038333+LeeJaeHyekk@users.noreply.github.com>
2025-05-30 08:49:37 +0900

    edit mainPage
    메인페이지 디자인 색상 변경

--- deukgeun/src/app/page.tsx
+++ deukgeun/src/app/page.tsx
@@ -10,15 +10,38 @@ import { Footer } from '@/widgets/Footer/Footer';
 
 const HomePage: FC = () => {
   return (
-    <main className={styles.main}>
+  <main className={styles.main}>
+  <section className={`${styles.section} ${styles.hero}`}>
+    <div className={styles.heroText}>
+      <h1>당신만의 운동 루틴을 지금 시작해보세요</h1>
+      <p>득근득근과 함께 목표를 정하고, 지속 가능한 변화를 만들어보세요.</p>
+    </div>
+    <div className={styles.heroImage}>
+      <img src="/health2.jpg"/>
+    </div>
+  </section>
 
-  <HeroSection />         
-  <FeatureSection />      
-  <PhilosophySection />
-  <CallToActionSection />
+  <section className={`${styles.section} ${styles.features}`}>
+    <FeatureSection />
+  </section>
+
+  <section className={`${styles.section} ${styles.philosophy}`}>
+    <PhilosophySection />
+  </section>
+
+  <section className={`${styles.section} ${styles.callToAction}`}>
+    <CallToActionSection />
+  </section>
+
   <Footer />
-    </main>
+</main>
+
+
+
+
   );
 };
 
 export default HomePage;
+
+
