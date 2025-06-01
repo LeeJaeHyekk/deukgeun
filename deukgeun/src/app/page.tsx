71bca3aeccba1b0929aaab8c2f33799891ca4f20
JaeHyuk <111038333+LeeJaeHyekk@users.noreply.github.com>
2025-06-02 08:57:40 +0900

    Revert "edit mainPage"
    This reverts commit a5ca511d86ba734af6341f0fdb6d6c0ea879ea50.

--- deukgeun/src/app/page.tsx
+++ deukgeun/src/app/page.tsx
@@ -10,38 +10,15 @@ import { Footer } from '@/widgets/Footer/Footer';
 
 const HomePage: FC = () => {
   return (
-  <main className={styles.main}>
-  <section className={`${styles.section} ${styles.hero}`}>
-    <div className={styles.heroText}>
-      <h1>당신만의 운동 루틴을 지금 시작해보세요</h1>
-      <p>득근득근과 함께 목표를 정하고, 지속 가능한 변화를 만들어보세요.</p>
-    </div>
-    <div className={styles.heroImage}>
-      <img src="/health2.jpg"/>
-    </div>
-  </section>
+    <main className={styles.main}>
 
-  <section className={`${styles.section} ${styles.features}`}>
-    <FeatureSection />
-  </section>
-
-  <section className={`${styles.section} ${styles.philosophy}`}>
-    <PhilosophySection />
-  </section>
-
-  <section className={`${styles.section} ${styles.callToAction}`}>
-    <CallToActionSection />
-  </section>
-
+  <HeroSection />         
+  <FeatureSection />      
+  <PhilosophySection />
+  <CallToActionSection />
   <Footer />
-</main>
-
-
-
-
+    </main>
   );
 };
 
 export default HomePage;
-
-
